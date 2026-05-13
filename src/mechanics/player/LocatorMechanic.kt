6d364600729617bd6fabc47062ea0505cc5b1256
illyrius666 <28700752+illyrius666@users.noreply.github.com>
2026-05-13 10:54:07 +0200

    improve locator to return current color
    Signed-off-by: illyrius666 <28700752+illyrius666@users.noreply.github.com>

--- src/mechanics/player/LocatorMechanic.kt
+++ src/mechanics/player/LocatorMechanic.kt
@@ -4,26 +4,33 @@ package org.xodium.illyriaplus.mechanics.player
 
 import io.papermc.paper.command.brigadier.Commands
 import io.papermc.paper.command.brigadier.argument.ArgumentTypes
-import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.format.NamedTextColor
 import net.kyori.adventure.text.format.TextColor
+import net.kyori.adventure.text.minimessage.tag.resolver.Placeholder
 import org.bukkit.Color
 import org.bukkit.entity.Player
 import org.bukkit.permissions.Permission
 import org.bukkit.permissions.PermissionDefault
 import org.xodium.illyriaplus.IllyriaPlus.Companion.instance
 import org.xodium.illyriaplus.Utils.CommandUtils.playerExecuted
+import org.xodium.illyriaplus.Utils.MM
 import org.xodium.illyriaplus.data.CommandData
 import org.xodium.illyriaplus.interfaces.MechanicInterface
 
 /** Represents a mechanic handling locator functionality within the system. */
 internal object LocatorMechanic : MechanicInterface {
+    private const val CURRENT_LOCATOR_MSG = "<gray>Current Locator color: </gray><hex>■"
+    private const val NEW_LOCATOR_MSG = "<gray>New Locator color: </gray><hex>■"
+    private const val DEFAULT_LOCATOR_MSG = "<gray>Locator color: default/not custom set</gray>"
+    private const val RESET_LOCATOR_MSG = "<gray>Locator color has been reset!</gray>"
+
     override val cmds =
         listOf(
             CommandData(
                 Commands
                     .literal("locator")
                     .requires { it.sender.hasPermission(perms[0]) }
+                    .playerExecuted { player, _ -> player.locator() }
                     .then(
                         Commands
                             .argument("color", ArgumentTypes.namedColor())
@@ -39,7 +46,7 @@ internal object LocatorMechanic : MechanicInterface {
                     ).then(
                         Commands
                             .literal("reset")
-                            .playerExecuted { player, _ -> player.locator() },
+                            .playerExecuted { player, _ -> player.locator(null) },
                     ),
                 "Allows players to personalise their locator bar",
                 listOf("lc"),
@@ -55,13 +62,27 @@ internal object LocatorMechanic : MechanicInterface {
             ),
         )
 
+    /** Shows the current locator color. */
+    private fun Player.locator() =
+        sendActionBar(
+            waypointColor?.let {
+                MM.deserialize(CURRENT_LOCATOR_MSG, Placeholder.styling("hex", TextColor.color(it.asRGB())))
+            }
+                ?: MM.deserialize(DEFAULT_LOCATOR_MSG),
+        )
+
     /**
      * Sets waypoint color.
      *
-     * @param color Optional color.
+     * @param color The color to apply, or null to reset.
      */
-    private fun Player.locator(color: TextColor? = null) {
+    private fun Player.locator(color: TextColor?) {
         waypointColor = color?.let { Color.fromRGB(it.value()) }
-        sendActionBar(Component.text("Locator color changed!", color))
+        sendActionBar(
+            color?.let {
+                MM.deserialize(NEW_LOCATOR_MSG, Placeholder.styling("hex", it))
+            }
+                ?: MM.deserialize(RESET_LOCATOR_MSG),
+        )
     }
 }
