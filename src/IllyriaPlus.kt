41868185a99586a9bbe0b39d483c706ce20cdc84
illyrius666 <28700752+illyrius666@users.noreply.github.com>
2026-06-04 10:26:14 +0200

    +
    Signed-off-by: illyrius666 <28700752+illyrius666@users.noreply.github.com>

--- src/IllyriaPlus.kt
+++ src/IllyriaPlus.kt
@@ -26,10 +26,10 @@ internal class IllyriaPlus : JavaPlugin() {
     override fun onEnable() {
         instance = this
 
-        val unsupportedVersionMsg =
-            "This plugin requires a supported server version. Supported versions: ${pluginMeta.version}."
-
-        if (!server.version.contains(pluginMeta.version.substringBefore("+"))) disablePlugin(unsupportedVersionMsg)
+        if (!server.version.contains(pluginMeta.version.substringBefore("+"))) {
+            logger.severe("This plugin requires the following supported version: ${pluginMeta.version}.")
+            server.pluginManager.disablePlugin(instance)
+        }
 
         val recipes =
             listOf(
@@ -118,16 +118,5 @@ internal class IllyriaPlus : JavaPlugin() {
         logger.info(
             "Registered: ${guis.size} gui's | Took ${guis.sumOf { it.register() }}ms",
         )
-    }
-
-    /**
-     * Disable the plugin and log the message.
-     *
-     * @param msg The message to log.
-     */
-    private fun disablePlugin(msg: String): Nothing {
-        logger.severe(msg)
-        server.pluginManager.disablePlugin(instance)
-        throw IllegalStateException(msg)
     }
 }
